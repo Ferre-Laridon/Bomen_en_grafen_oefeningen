65a7e34eb8c7a299fb8a5f1fb931a4b171f86e14
Ferre Laridon <ferre.laridon@student.ucll.be>
2022-03-01 11:21:56 +0100

    oefeningen les2 afgewerkt

--- week01_recursie/src/Recursie.java
+++ week01_recursie/src/Recursie.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.IllformedLocaleException;
 import java.util.List;
 
 public class Recursie {
@@ -7,60 +8,100 @@ public class Recursie {
     // Oefening 1: n-de Fibonacci-getal
 
     public static int fibonacci(int getal) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (getal <=0) throw new IllegalArgumentException();
+        if (getal == 1 || getal == 2) return 1;
+        return fibonacci(getal-1) + fibonacci(getal-2);
     }
 
     // Oefening 2 : som van cijfers
 
     public static int somVanCijfers(int getal) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (getal/10 == 0) return Math.abs(getal);
+        getal = Math.abs(getal);
+        return getal%10 + somVanCijfers((getal-getal%10)/10);
     }
 
     // Oefening 3: keer een string om
     public static String keerOm(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (s==null) throw new IllegalArgumentException();
+        if (s.isBlank()) return "";
+        if (s.length()==1) return s;
+        return s.substring(s.length()-1) + keerOm(s.substring(0, s.length()-1));
     }
 
     //oefening 4: countX
 
     public static int countX(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (s==null) throw new IllegalArgumentException();
+        if (s.isBlank()) return 0;
+        String letter = s.substring(0,1);
+        if (letter.equals("x")) return 1 + countX(s.substring(1));
+        return countX(s.substring(1));
     }
 
     //oefening 5 : countHi
     public static int countHi(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (s==null) throw new IllegalArgumentException();
+        if (s.isBlank()) return 0;
+        if (s.length()==1) return 0;
+        String letter = s.substring(0,1);
+        String letter2 = s.substring(1,2);
+        if (letter.equals("h") && letter2.equals("i")) return 1 + countHi(s.substring(1));
+        return countHi(s.substring(1));
     }
 
     // oefening 6
     public static String changeXY(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (s==null) throw new IllegalArgumentException();
+        if (s.isBlank()) return "";
+        String letter = s.substring(0,1);
+        if (letter.equals("x")) return "y" + changeXY(s.substring(1));
+        return letter + changeXY(s.substring(1));
     }
 
     // oefening 7
 
     public static String changePi(String s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (s==null) throw new IllegalArgumentException();
+        if (s.isBlank()) return "";
+        if (s.length()==1) return s;
+        String letter = s.substring(0,1);
+        String letter2 = s.substring(1,2);
+        if (letter.equals("p") && letter2.equals("i")) return "3.14" + changePi(s.substring(2));
+        return letter + changePi(s.substring(1));
     }
 
     // oefening 8:
     public static int tweelog(int getal) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (getal<=0) throw new IllegalArgumentException();
+        if (getal==1) return 0;
+        return 1 + tweelog(getal/2);
     }
 
     // oefening 9;
     public static double findMaximum(List<Double> lijst) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (lijst==null || lijst.size()==0) throw new IllegalArgumentException();
+        if (lijst.size()==1) return lijst.get(0);
+        double getal1 = lijst.get(0);
+        double getal2 = lijst.get(1);
+        if (getal1>getal2) lijst.remove(1);
+        else lijst.remove(0);
+        return findMaximum(lijst);
     }
 
     // oefening 10;
     public static ArrayList<String> findSubstrings(String string) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        ArrayList<String> resultaat = new ArrayList<>();
+        if (string==null) throw new IllegalArgumentException();
+        //if (string.isBlank()) return resultaat.add("");
+        return new ArrayList<>();
     }
 
     // oefening 11;
-    public static int aantalKaarten(int n){
-        throw new UnsupportedOperationException("Not yet implemented");
+    public static int aantalKaarten(int n) {
+        if (n<=0) throw new IllegalArgumentException();
+        if (n==1) return 2;
+        return 2 + (n-1)*3 + aantalKaarten(n-1);
     }
 
 }
--- week02_bomen/src/domain/BinaryTree.java
+++ week02_bomen/src/domain/BinaryTree.java
@@ -1,26 +1,92 @@
 package domain;
 
 public class BinaryTree<E> {
-	private E data;
-	private BinaryTree<E> leftTree, rightTree;
-	
-	public BinaryTree(E data){
-		this(data,null,null);
-	}
-	
-	public BinaryTree(E data, BinaryTree<E> leftTree, BinaryTree<E> rightTree){
-		if (data == null) {
-			throw new IllegalArgumentException();
-		}
-		this.data= data;
-		this.leftTree= leftTree;
-		this.rightTree= rightTree;
-	}
-	
-	public void printPreorder(){
-			System.out.print(this.data + " ");
-			if (this.leftTree != null) this.leftTree.printPreorder();
-			if (this.rightTree != null) this.rightTree.printPreorder();
-	}
+    private E data;
+    private BinaryTree<E> leftTree, rightTree;
+
+    public BinaryTree(E data) {
+        this(data, null, null);
+    }
+
+    public BinaryTree(E data, BinaryTree<E> leftTree, BinaryTree<E> rightTree) {
+        if (data == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.data = data;
+        this.leftTree = leftTree;
+        this.rightTree = rightTree;
+    }
+
+    public void printPreorder() {
+        System.out.print(this.data + " ");
+        if (this.leftTree != null) this.leftTree.printPreorder();
+        if (this.rightTree != null) this.rightTree.printPreorder();
+    }
+
+    public void printInorder() {
+        if (this.leftTree != null) this.leftTree.printInorder();
+        System.out.print(this.data + " ");
+        if (this.rightTree != null) this.rightTree.printInorder();
+    }
+
+    public void printPostorder() {
+        if (this.leftTree != null) this.leftTree.printPostorder();
+        if (this.rightTree != null) this.rightTree.printPostorder();
+        System.out.print(this.data + " ");
+    }
+
+    public int countNodes() {
+        if (this.leftTree == null && this.rightTree == null) return 1;
+        else if (this.leftTree != null && this.rightTree == null) return 1 + this.leftTree.countNodes();
+        else if (this.leftTree == null) return 1 + this.rightTree.countNodes();
+        else return 1 + this.leftTree.countNodes() + this.rightTree.countNodes();
+    }
+
+    public int getDepth() {
+        if (this.leftTree == null && this.rightTree == null) return 1;
+        else if (this.leftTree != null && this.rightTree == null) return 1 + this.leftTree.getDepth();
+        else if (this.leftTree == null) return 1 + this.rightTree.getDepth();
+        else return 1 + Math.max(this.leftTree.getDepth(), this.rightTree.getDepth());
+    }
+
+    public boolean isLeaf() {
+        return (this.leftTree == null && this.rightTree == null);
+    }
+
+    public int countLeaves() {
+        if (this.leftTree == null && this.rightTree == null) return 1;
+        else if (this.leftTree == null) return this.rightTree.countLeaves();
+        else if (this.rightTree == null) return this.leftTree.countLeaves();
+        else if (this.leftTree.isLeaf()) return 1 + this.rightTree.countLeaves();
+        else if (this.rightTree.isLeaf()) return 1 + this.leftTree.countLeaves();
+        else return this.leftTree.countLeaves() + this.rightTree.countLeaves();
+    }
+
+    public void getDataLeaves() {
+        if (this.leftTree == null && this.rightTree == null) System.out.print(this.data + " ");
+        else if (this.leftTree == null) this.rightTree.getDataLeaves();
+        else if (this.rightTree == null) this.leftTree.getDataLeaves();
+        else if (this.leftTree.isLeaf()) {
+            System.out.print(this.leftTree.data + " ");
+            this.rightTree.getDataLeaves();
+        } else if (this.rightTree.isLeaf()){
+            System.out.print(this.rightTree.data + " ");
+            this.leftTree.getDataLeaves();
+        }
+        else {
+            this.leftTree.getDataLeaves();
+            this.rightTree.getDataLeaves();
+        }
+    }
+
+    public boolean contains(E data) {
+        if (this.data == data) return true;
+        else if (this.rightTree != null && this.leftTree != null)
+            return (this.leftTree.contains(data) || this.rightTree.contains(data));
+        else if (this.leftTree == null && this.rightTree != null) return this.rightTree.contains(data);
+        else if (this.leftTree != null) return this.leftTree.contains(data);
+        else return false;
+    }
 
 }
